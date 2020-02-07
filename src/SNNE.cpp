67cce94b89f71883bf5c104c75c5cae6d184867e
Caio Piccirillo <caio.piccirillo@tecsidel.com>
2020-02-07 18:02:05 -0300

    Functions added. Constructor changed for setting mode separately.

--- src/SNNE.cpp
+++ src/SNNE.cpp
@@ -1,10 +1,33 @@
 #include "SNNE.h"
 
-SNNE::SNNE(int number_of_neurons, SNNE::Mode mode)
+SNNE::SNNE(int number_of_neurons)
 {
-    
+    this->neuron_output.resize(number_of_neurons);
+    this->neuron_trace.resize(number_of_neurons);
 }
 
 SNNE::~SNNE()
 {
+}
+
+void SNNE::Start()
+{
+}
+
+void SNNE::Stop()
+{
+}
+
+void SNNE::SetMode(int neuron_position, SNNE::Mode mode)
+{
+    // Check if position exists
+    if (neuron_position > this->neuron_mode.size())
+    {
+        std::cout << "No neuron with such position" << std::endl;
+        return;
+    }
+    else
+    {
+        this->neuron_mode[(std::vector<int>::size_type)neuron_position] = mode;
+    }
 }
--- src/SNNE.h
+++ src/SNNE.h
@@ -1,10 +1,17 @@
 #ifndef _SNNENCODER_H
 #define _SNNENCODER_H
-
+#include <iostream>
+#include <vector>
 class SNNE
 {
 private:
-    
+    // 
+    std::vector<int> neuron_trace;
+    //
+    std::vector<int> neuron_output; 
+    //
+    std::vector<int> neuron_mode; 
+
 public:
     enum Mode
     {
@@ -12,8 +19,20 @@ public:
         Phase,
         Population
     };
+    struct Output
+    {
+        //
+        std::vector<int> neuron_trace;
+        //
+        std::vector<int> neuron_output; 
+    };
 
-    SNNE(int number_of_neurons, SNNE::Mode mode);
+    void Start();
+    void Stop();
+    // Function used to set mode of operation
+    void SetMode(int neuron_position, SNNE::Mode neuron_mode);
+
+    SNNE(int number_of_neurons);
     ~SNNE();
 };
 
